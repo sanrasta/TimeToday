7ebdc0c6e7ec65157d98eed8e3182e4b03dc3c71
sanrasta <sanrasta@users.noreply.github.com>
2025-04-05 15:46:39 -0500

    Initial Commit

---
+++ TimeToday/TimeTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTodayApp.swift
+//  TimeToday
+//
+//  Created by Santiago on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TimeTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeTodayTests/TimeTodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTodayTests.swift
+//  TimeTodayTests
+//
+//  Created by Santiago on 4/5/25.
+//
+
+import Testing
+@testable import TimeToday
+
+struct TimeTodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
